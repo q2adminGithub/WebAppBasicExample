feb4d340b051b1acb4cda8a6d63e5a1d053fa299
q2rb <156476595+q2rb@users.noreply.github.com>
2024-03-01 09:26:20 +0100

    creating new table inputdata which is populated with data from file located in backend/assets/inputdata/inputdata.txt

--- init.sql
+++ init.sql
@@ -9,9 +9,17 @@ stateid SERIAL NOT NULL PRIMARY KEY,
 ts_utc TIMESTAMP NOT NULL,
 statejson JSON NOT NULL
 );
+
+CREATE TABLE inputdata(
+id SERIAL NOT NULL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+datajson JSON NOT NULL
+);
+
 GRANT ALL ON ALL TABLES IN SCHEMA "public" TO myuser;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA "public" TO myuser;
 
+COPY inputdata(name, datajson) FROM '/var/lib/postgresql/data/inputdata.txt' CSV QUOTE e'\x01' DELIMITER '|';
 
 \c mydb_test;
 CREATE TABLE statesave(
@@ -19,5 +27,14 @@ stateid SERIAL NOT NULL PRIMARY KEY,
 ts_utc TIMESTAMP NOT NULL,
 statejson JSON NOT NULL
 );
+
+CREATE TABLE inputdata(
+id SERIAL NOT NULL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+datajson JSON NOT NULL
+);
+
 GRANT ALL ON ALL TABLES IN SCHEMA "public" TO myuser;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA "public" TO myuser;
+
+COPY inputdata(name, datajson) FROM '/var/lib/postgresql/data/inputdata.txt' CSV QUOTE e'\x01' DELIMITER '|';
