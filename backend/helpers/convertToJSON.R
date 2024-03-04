0adc5076bda681614edd22e7ff0e52b467d2fe5c
q2rb <156476595+q2rb@users.noreply.github.com>
2024-03-04 09:00:07 +0100

    conversion from .rda to JSON as a separate function with input folder, output folder, output filename, delimiter as arguments.Checks for data.frame objects now

--- backend/helpers/convertToJSON.R
+++ backend/helpers/convertToJSON.R
@@ -3,51 +3,63 @@ library(rlang)
 
 # stand-alone script to transform the .rda files containing the input tables into a file used for initial load during execution of init.sql
 # run this from a R installation on Linux (uses shell commands echo and cat)
+# any delimiter different from '|' must also be taken into account in the COPY statement in init.sql
 
-input_file_names <- base::list.files(
-    path = "../assets/inputdata",
-    pattern = ".rda",
-    full.names = TRUE,
-    recursive = TRUE
-)
+convertRDAtoJSON <- function(inputDirectory, outputDirectory, outputFilename, delimiter){
 
-ldf <- lapply(input_file_names, function(x){eval(rlang::sym(load(x)))})
-varnames <- sapply(input_file_names, load, USE.NAMES = FALSE)
-names(ldf) <- varnames
+    input_file_names <- base::list.files(
+        path = inputDirectory,
+        pattern = ".rda",
+        full.names = TRUE,
+        recursive = TRUE
+    )
 
-if (length(ldf) > 0){
+    ldf <- lapply(input_file_names, function(x){eval(rlang::sym(load(x)))})
+    varnames <- sapply(input_file_names, load, USE.NAMES = FALSE)
+    names(ldf) <- varnames
 
-    tmpDir <- "../assets/tmp/"
-    if (dir.exists(tmpDir) == FALSE){
-        dir.create(tmpDir)
-    }
+    if (length(ldf) > 0){
 
-    x <- c()
-    delimiter <- '|' # any change here must also be taken into account in the COPY statement in init.sql
-    delimiterPresent <- ''
-    for (dfname in ls(ldf)) {
-        jsonstring <- jsonlite::toJSON(ldf[[dfname]])
-
-        if (grepl(delimiter, jsonstring, fixed = TRUE)){
-            delimiterPresent <- dfname
+        tmpDir <- paste0(outputDirectory, '/tmp/')
+        if (dir.exists(tmpDir) == FALSE){
+            dir.create(tmpDir)
         }
-        outfilename <- paste0(tmpDir, dfname, '.txt')
-        newoutfilename <- paste0(tmpDir, dfname, '_modified.txt')
-        x <- append(x, newoutfilename)
-        jsonlite::write_json(ldf[[dfname]], outfilename)
+        finalOutput <- paste0(outputDirectory, '/', outputFilename)
+        x <- c()
+        for (dfname in ls(ldf)) {
+            if (is.data.frame(ldf[[dfname]]) == FALSE){
+                system(paste0('cat > ', finalOutput)) # creating a new empty file
+                unlink(tmpDir, recursive=TRUE, force=TRUE)
+                return(paste0('ERROR: the variable ', dfname, ' is not a data.frame ... creating empty output file'))
+            }
+            jsonstring <- jsonlite::toJSON(ldf[[dfname]])
 
-        system(paste0('{ echo -n "',dfname, delimiter,'"; cat ',outfilename,'; } >',newoutfilename)) # inserting the entry for name column with | as delimiter, see https://stackoverflow.com/a/26580151
-    }
-    if (nchar(delimiterPresent) == 0){
+            if (grepl(delimiter, jsonstring, fixed = TRUE)){
+                system(paste0('cat > ', finalOutput)) # creating a new empty file
+                unlink(tmpDir, recursive=TRUE, force=TRUE)
+                return(paste0('ERROR: the dataframe ', dfname, ' contains the delimiter ', delimiter, ' ... creating empty output file'))
+            }
+            outfilename <- paste0(tmpDir, dfname, '.txt')
+            newoutfilename <- paste0(tmpDir, dfname, '_modified.txt')
+            x <- append(x, newoutfilename)
+            jsonlite::write_json(ldf[[dfname]], outfilename)
+            system(paste0('{ echo -n "',dfname, delimiter,'"; cat ',outfilename,'; } >',newoutfilename)) # inserting the entry for name column with | as delimiter, see https://stackoverflow.com/a/26580151
+        }
         allfiles <- paste(x, collapse=' ')
-        system(paste0('cat ',allfiles,' > ../assets/inputdata/inputdata.txt'))
-        print('inputdata.txt successfully created in ../assets/inputdata')
+        system(paste0('cat ',allfiles,' > ', finalOutput))
+        ret <- paste0(outputFilename, ' successfully created in ', outputDirectory)
+        unlink(tmpDir, recursive=TRUE, force=TRUE)
     } else {
-        system(paste0('cat > ../assets/inputdata/inputdata.txt')) # creating a new empty file
-        print(paste0('ERROR: the delimiter ',delimiter,' is present in the input data for variable ',delimiterPresent,' ... creating empty inputdata.txt in ../assets/inputdata'))
+        ret <- paste0('ERROR: there are no input .rda files present ... creating empty output file')
+        system(paste0('cat > ', finalOutput)) # creating a new empty file
     }
-    unlink(tmpDir, recursive=TRUE, force=TRUE)
-} else {
-    print(paste0('ERROR: there are no input .rda files present ... creating empty inputdata.txt in ../assets/inputdata'))
-    system(paste0('cat > ../assets/inputdata/inputdata.txt')) # creating a new empty file
+    return(ret)
 }
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 4) {
+    stop("must provide 4 argument: inputDirectory outputDirectory, outputFilename, delimiter", call. = FALSE)
+} else {
+    ret <- convertRDAtoJSON(args[1], args[2], args[3], args[4])
+}
+print(ret)
